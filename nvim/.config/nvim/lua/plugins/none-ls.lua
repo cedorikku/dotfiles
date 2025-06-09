50f221c56f661ee78d097f88a4af1892b113cf94
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-06-09 20:19:40 +0800

    remove slow file types when formatting on save

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -9,16 +9,16 @@ return {
   },
   config = function()
     local null_ls = require 'null-ls'
-    local formatting = null_ls.builtins.formatting -- to setup formatters
+    local formatting = null_ls.builtins.formatting   -- to setup formatters
     local diagnostics = null_ls.builtins.diagnostics -- to setup linters
 
     -- Formatters & linters for mason to install
     require('mason-null-ls').setup {
       ensure_installed = {
-        'prettier', -- ts/js formatter
-        'stylua', -- lua formatter
-        'eslint_d', -- ts/js linter
-        'shfmt', -- Shell formatter
+        'prettier',  -- ts/js formatter
+        'stylua',    -- lua formatter
+        'eslint_d',  -- ts/js linter
+        'shfmt',     -- Shell formatter
         'checkmake', -- linter for Makefiles
       },
       automatic_installation = true,
@@ -26,7 +26,7 @@ return {
 
     local sources = {
       diagnostics.checkmake,
-      formatting.prettier.with { filetypes = { 'html', 'json', 'yaml', 'markdown' } },
+      formatting.prettier.with { filetypes = { 'html', 'css', 'json', 'yaml', 'markdown' } },
       formatting.stylua,
       formatting.shfmt.with { args = { '-i', '4' } },
     }
