2c5ca677cef43e585e43b83cb28ed60c4df366bf
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-06-10 14:12:11 +0800

    remove auto formatting

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -9,16 +9,16 @@ return {
   },
   config = function()
     local null_ls = require 'null-ls'
-    local formatting = null_ls.builtins.formatting   -- to setup formatters
+    local formatting = null_ls.builtins.formatting -- to setup formatters
     local diagnostics = null_ls.builtins.diagnostics -- to setup linters
 
     -- Formatters & linters for mason to install
     require('mason-null-ls').setup {
       ensure_installed = {
-        'prettier',  -- ts/js formatter
-        'stylua',    -- lua formatter
-        'eslint_d',  -- ts/js linter
-        'shfmt',     -- Shell formatter
+        'prettier', -- ts/js formatter
+        'stylua', -- lua formatter
+        'eslint_d', -- ts/js linter
+        'shfmt', -- Shell formatter
         'checkmake', -- linter for Makefiles
       },
       automatic_installation = true,
@@ -26,33 +26,20 @@ return {
 
     local sources = {
       diagnostics.checkmake,
-      formatting.prettier.with { filetypes = { 'html', 'css', 'json', 'yaml', 'markdown' } },
+      formatting.prettier.with { filetypes = { 'html', 'css', 'javascript', 'javascriptreact', 'json', 'yaml', 'markdown' } },
       formatting.stylua,
       formatting.shfmt.with { args = { '-i', '4' } },
     }
 
-    local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
     null_ls.setup {
       -- debug = true, -- Enable debug mode. Inspect logs with :NullLsLog.
       sources = sources,
       -- you can reuse a shared lspconfig on_attach callback here
       on_attach = function(client, bufnr)
         if client.supports_method 'textDocument/formatting' then
-          vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-          vim.api.nvim_create_autocmd('BufWritePre', {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format { async = false }
-            end,
-          })
-
           -- Set personal formatting keymap
           vim.bo[bufnr].formatexpr = 'v:lua.vim.lsp.formatexpr()'
-          vim.keymap.set('n', '<leader>p', '<cmd>lua vim.lsp.buf.format({ async = false })<CR>',
-            { noremap = true, silent = true })
-        else
-          vim.bo[bufnr].formatexpr = nil
+          vim.keymap.set('n', '<leader>p', '<cmd>lua vim.lsp.buf.format({ async = false })<CR>', { noremap = true, silent = true, desc = 'Format with lsp' })
         end
       end,
     }
