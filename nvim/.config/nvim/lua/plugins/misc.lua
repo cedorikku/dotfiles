a552bc07882b5373f4099a152364a24dd7c91d28
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-06-11 21:07:32 +0800

    add tag auto pairing in config

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -28,6 +28,20 @@ return {
     opts = {},
   },
   {
+    -- Autoclose usual markup language tags (html, xml, etc)
+    'windwp/nvim-ts-autotag',
+    opts = {
+      enable_close = true, -- Auto close tags
+      enable_rename = true, -- Auto rename pairs of tags
+      enable_close_on_slash = false, -- Auto close on trailing </
+    },
+    per_filetype = {
+      -- ['html'] = {
+      --     enable_close = false,
+      -- },
+    },
+  },
+  {
     -- Highlight todo, notes, etc in comments
     'folke/todo-comments.nvim',
     event = 'VimEnter',
@@ -45,7 +59,7 @@ return {
     -- Clipboard manager
     'AckslD/nvim-neoclip.lua',
     dependencies = {
-      {'nvim-telescope/telescope.nvim'},
+      { 'nvim-telescope/telescope.nvim' },
       -- {'ibhagwan/fzf-lua'},
     },
     config = function()
