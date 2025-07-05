60f7dc2f97b91da02b0641d65a7054eede2788a1
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-07-05 18:17:57 +0800

    ensure using master branch

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,8 @@
-return { -- Highlight, edit, and navigate code
+return {
   'nvim-treesitter/nvim-treesitter',
+  lazy = 'false',
+  branch = 'master',
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   opts = {
     ensure_installed = {
       'html',
@@ -25,13 +26,13 @@ return { -- Highlight, edit, and navigate code
     highlight = { enable = true },
     indent = { enable = true },
   },
-
-  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-  --
-  -- There are additional nvim-treesitter modules that you can use to interact
-  -- with nvim-treesitter. You should go explore a few and see what interests you:
-  --
-  --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-  --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 }
+
+-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+--
+-- There are additional nvim-treesitter modules that you can use to interact
+-- with nvim-treesitter. You should go explore a few and see what interests you:
+--
+--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
