6a5e438db2fd7cd39bfcc239ff10aa23b6b14b11
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-07-05 17:59:06 +0800

    remove legacy opts to remove a warning

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -35,16 +35,6 @@ return {
   {
     -- Autoclose usual markup language tags (html, xml, etc)
     'windwp/nvim-ts-autotag',
-    opts = {
-      enable_close = true,           -- Auto close tags
-      enable_rename = true,          -- Auto rename pairs of tags
-      enable_close_on_slash = false, -- Auto close on trailing </
-    },
-    per_filetype = {
-      -- ['html'] = {
-      --     enable_close = false,
-      -- },
-    },
   },
   {
     -- Highlight todo, notes, etc in comments
