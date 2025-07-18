a0253035af05d0cffd7ca24e5b26ef3ee908b7bc
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-07-18 20:27:36 +0800

    adopt some of zsh config from cachyos' defaults

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,6 +1,12 @@
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+alias c="clear"
+alias fixpacman="sudo rm /var/lib/pacman/db.lck" # from cachyos defaults
+alias cleanch="sudo pacman -Scc" # So that I don't forget this exists
+
 # common
 alias shutdown='sudo shutdown now'
 alias restart='sudo reboot'
-alias c="clear"
 
 alias ls="eza --icons"
--- zsh/.zshenv
+++ zsh/.zshenv
@@ -1,4 +1,7 @@
 export TERM=xterm-256color
+
+# Path to your oh-my-zsh installation.
+export ZSH="/usr/share/oh-my-zsh"
 export ZSH_THEME="edvardm"
 
 # Use Neovim as default editor
