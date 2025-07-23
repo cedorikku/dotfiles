ee8c3d132d4f7c6c72085fb270b4c097e2e3984f
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-07-23 15:06:06 +0800

    Revert "remove window navigation keymaps"
    This reverts commit 55b5d2ac2c19aaaf3f6b1971b15317e5aed0124a.

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -34,6 +34,12 @@ vim.keymap.set('n', '<leader>h', '<C-w>s', opts)      -- split window horizontal
 vim.keymap.set('n', '<leader>se', '<C-w>=', opts)     -- make split windows equal width & height
 vim.keymap.set('n', '<leader>xs', ':close<CR>', opts) -- close current split window
 
+-- Navigate between splits
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', opts)
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', opts)
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', opts)
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', opts)
+
 -- Toggle line wrapping
 vim.keymap.set('n', '<leader>lw', '<cmd>set wrap!<CR>', opts)
 
