f8e21790fac9ea163bad380d6a7991e1a7a78df4
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-09-06 10:30:57 +0800

    feat: tweak oil keymaps to not interfere with split navigation

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -7,6 +7,13 @@ return {
     view_options = {
       show_hidden = true,
     },
+    keymaps = {
+      ["<C-h>"] = false,
+      ["<C-l>"] = false,
+      ["<C-e>"] = "actions.refresh",
+      ["<C-s>"] = { "actions.select", opts = { horizontal = true } },
+      ["<C-t>"] = { "actions.select", opts = { vertical = true } },
+    }
   },
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
