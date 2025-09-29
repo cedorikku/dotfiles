76bb03b3d9030754665dafea4534c0235fbbe71e
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-09-29 22:03:10 +0800

    chore(nvim): document  vscode maps

--- nvim/.config/nvim/lua/plugins/vscode.lua
+++ nvim/.config/nvim/lua/plugins/vscode.lua
@@ -10,37 +10,17 @@ local opts = { noremap = true, silent = true }
 -- Disable the spacebar key's default behavior in Normal and Visual modes
 keymap({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
--- keymap('n', '<C-u>', function()
---   vscode.call 'vscode-neovim.ctrl-u'
---   vscode.call 'vscode-neovim.z'
--- end)
+------------
+-- Editor --
+------------
 
-keymap('n', '<leader>v', function()
-  vscode.action 'workbench.action.splitEditorRight'
-end)
-
-keymap('n', '<leader>h', function()
-  vscode.action 'workbench.action.splitEditorDown'
-end)
-
-keymap('n', '<leader>sf', function()
-  vscode.action 'workbench.action.quickOpen'
-end)
-keymap('n', '<leader>sg', function()
-  vscode.action 'workbench.action.findInFiles'
-end)
-
+-- Formatting
 keymap('n', '<leader>p', function()
   vscode.call 'editor.action.formatDocument'
   vscode.notify 'Code formatted'
 end)
 
-keymap({ 'n', 'v' }, '<leader>t', function()
-  vscode.action 'workbench.action.terminal.toggleTerminal'
-end)
-keymap({ 'n', 'v' }, '<leader>b', function()
-  vscode.action 'editor.debug.action.toggleBreakpoint'
-end)
+-- Diagnostics
 keymap({ 'n', 'v' }, '<leader>d', function()
   vscode.call 'editor.action.showHover'
 end)
@@ -48,6 +28,24 @@ keymap({ 'n', 'v' }, '<leader>q', function()
   vscode.action 'workbench.actions.view.problems'
 end)
 
+-- Debugging
+keymap({ 'n', 'v' }, '<leader>b', function()
+  vscode.action 'editor.debug.action.toggleBreakpoint'
+end)
+
+-------------
+-- Windows --
+-------------
+
+-- Splits
+keymap('n', '<leader>v', function()
+  vscode.action 'workbench.action.splitEditorRight'
+end)
+keymap('n', '<leader>h', function()
+  vscode.action 'workbench.action.splitEditorDown'
+end)
+
+-- Navigation
 keymap({ 'n', 'v' }, '<C-j>', function()
   vscode.action 'workbench.action.navigateDown'
 end)
@@ -59,6 +57,19 @@ keymap({ 'n', 'v' }, '<C-h>', function()
 end)
 keymap({ 'n', 'v' }, '<C-l>', function()
   vscode.action 'workbench.action.navigateRight'
+end)
+
+-- Terminal
+keymap({ 'n', 'v' }, '<leader>t', function()
+  vscode.action 'workbench.action.terminal.toggleTerminal'
+end)
+
+-- Finder
+keymap('n', '<leader>sf', function()
+  vscode.action 'workbench.action.quickOpen'
+end)
+keymap('n', '<leader>sg', function()
+  vscode.action 'workbench.action.findInFiles'
 end)
 
 -- File Explorer
