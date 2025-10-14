3904576d3b0eb6c81b61f15c60d3b7d85a317763
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-10-14 16:50:26 +0800

    chore: format

--- nvim/.config/nvim/lua/plugins/auto-session.lua
+++ nvim/.config/nvim/lua/plugins/auto-session.lua
@@ -1,20 +1,20 @@
 return {
-    'rmagatti/auto-session',
-    lazy = false,
-    keys = {
-        { '<leader>wf', '<cmd>AutoSession search<CR>',  { desc = 'Session picker' } },
-        { '<leader>ws', '<cmd>AutoSession save<CR>',    { desc = 'Save session' } },
-        { '<leader>wd', '<cmd>AutoSession delete<CR>',  { desc = 'Delete session' } },
-        { '<leader>wt', '<cmd>AutoSession toggle<CR>',  { desc = 'Session autosave toggle' } },
-        { '<leader>wr', '<cmd>AutoSession restore<CR>', { desc = 'Session restore on cwd (manual)' } },
-    },
+  'rmagatti/auto-session',
+  lazy = false,
+  keys = {
+    { '<leader>wf', '<cmd>AutoSession search<CR>', { desc = 'Session picker' } },
+    { '<leader>ws', '<cmd>AutoSession save<CR>', { desc = 'Save session' } },
+    { '<leader>wd', '<cmd>AutoSession delete<CR>', { desc = 'Delete session' } },
+    { '<leader>wt', '<cmd>AutoSession toggle<CR>', { desc = 'Session autosave toggle' } },
+    { '<leader>wr', '<cmd>AutoSession restore<CR>', { desc = 'Session restore on cwd (manual)' } },
+  },
 
-    ---enables autocomplete for opts
-    ---@module "auto-session"
-    ---@type AutoSession.Config
-    opts = {
-        enabled = true,
-        lsp_stop_on_restore = true,
-        suppressed_dirs = { '~/', '~/Downloads', '~/Desktop', '~/Documents', '/' },
-    },
+  ---enables autocomplete for opts
+  ---@module "auto-session"
+  ---@type AutoSession.Config
+  opts = {
+    enabled = true,
+    lsp_stop_on_restore = true,
+    suppressed_dirs = { '~/', '~/Downloads', '~/Desktop', '~/Documents', '/' },
+  },
 }
