b4512fdcd4492f260e3921ddab0a8fc3c5aa4775
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-12-17 17:03:08 +0800

    fix(zsh): incompatible commands for non-arch distro/linux OS

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -2,9 +2,3 @@
 # plugins, and themes. Aliases can be placed here, though oh-my-zsh
 # users are encouraged to define aliases within the ZSH_CUSTOM folder.
 alias c="clear"
-alias fixpacman="sudo rm /var/lib/pacman/db.lck" # from cachyos defaults
-alias cleanch="sudo pacman -Scc" # So that I don't forget this exists
-
-# common
-alias shutdown='sudo shutdown now'
-alias restart='sudo reboot'
