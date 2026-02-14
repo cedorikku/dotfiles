b810920fe6125747c342506a18eb98bd17fbf738
Cedric <75742716+cedorikku@users.noreply.github.com>
2026-02-14 21:54:56 +0800

    feat(nvim): replace surround plugin for windows

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -47,7 +47,14 @@ else
     { 'nvim-mini/mini.comment' },
     { 'nvim-mini/mini.move' },
     { 'nvim-mini/mini.pairs' },
-    { 'nvim-mini/mini.surround' },
+    {
+      "kylechui/nvim-surround",
+      version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+      event = "VeryLazy",
+      config = function()
+        require("nvim-surround").setup()
+      end
+    },
     { 'JoosepAlviste/nvim-ts-context-commentstring' },
     { 'tpope/vim-repeat' },
     { 'gbprod/yanky.nvim' },
