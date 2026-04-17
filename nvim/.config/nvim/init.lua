a78426a5c8e1bab836d342a87ea7195ee8eece97
Cedric <75742716+cedorikku@users.noreply.github.com>
2026-04-17 18:05:31 +0800

    nvim: remove unused plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -42,18 +42,17 @@ else
   require('lazy').setup {
     require 'plugins.treesitter',
     { 'ggandor/flit.nvim' },
-    { 'ggandor/leap.nvim' },
     { 'nvim-mini/mini.ai' },
     { 'nvim-mini/mini.comment' },
     { 'nvim-mini/mini.move' },
     { 'nvim-mini/mini.pairs' },
     {
-      "kylechui/nvim-surround",
-      version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
-      event = "VeryLazy",
+      'kylechui/nvim-surround',
+      version = '^3.0.0', -- Use for stability; omit to use `main` branch for the latest features
+      event = 'VeryLazy',
       config = function()
-        require("nvim-surround").setup()
-      end
+        require('nvim-surround').setup()
+      end,
     },
     { 'JoosepAlviste/nvim-ts-context-commentstring' },
     { 'tpope/vim-repeat' },
