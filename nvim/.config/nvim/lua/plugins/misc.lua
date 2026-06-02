7cc47c5742e31fb5b95b7a70fb3a0d4b56d66c6b
Cedric <75742716+cedorikku@users.noreply.github.com>
2026-06-02 19:23:24 +0800

    chore(nvim): remove dupe plugin ref with update

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -4,14 +4,6 @@ return {
     'tpope/vim-repeat',
   },
   {
-    'kylechui/nvim-surround',
-    version = '^4.0.0', -- Use for stability; omit to use `main` branch for the latest features
-    event = 'VeryLazy',
-    config = function()
-      require('nvim-surround').setup()
-    end,
-  },
-  {
     -- Tmux & split window navigation
     'aserowy/tmux.nvim',
     opts = {
@@ -50,10 +42,7 @@ return {
   {
     -- Easily surround text
     'kylechui/nvim-surround',
-    version = '^3.0.0', -- Use for stability; omit to use `main` branch for the latest features
+    version = '^4.0.0', -- Use for stability; omit to use `main` branch for the latest features
     event = 'VeryLazy',
-    config = function()
-      require('nvim-surround').setup {}
-    end,
   },
 }
